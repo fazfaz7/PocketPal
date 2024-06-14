92bfdb948cf8d8d7fba0292f3e5fb572fb9e4708
fazfaz7 <adrian.faz123@gmail.com>
2024-06-14 16:50:30 -0600

    Initial Commit

---
+++ PocketPal/PocketPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPalApp.swift
+//  PocketPal
+//
+//  Created by Adrian Faz on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
